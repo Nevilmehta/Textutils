63561855a5273afc141c2c4e4f32229b64055568
Nevil Mehta <nevilmehta5@gmail.com>
2022-08-03 17:56:34 +0530

    about and textbox

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,21 +7,51 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText)
   }
+  const handleLoClick = ()=>{
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+  const handleclClick = ()=>{
+    let newText = "";
+    setText(newText)
+  }
+  const handlecopyClick = ()=>{
+    var text= document.getElementById("myBox")
+    text.select()
+    navigator.clipboard.writeText(text.value)
+  }
+  const handlermspceClick = ()=>{
+    let newText = text.split(/[ ]+/)
+    setText(newText.join(" "))
+  }
   const handleOnChange = (event)=>{
     // console.log("On Change")
     setText(event.target.value)
   }
-    
-  const [text, setText] = useState('Enter the text');  
+  
+  const [text, setText] = useState('');  
 //   text = "new text"; wrong way
 //   setText("new text"); correct way
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleclClick}>Clear</button>
+        <button className="btn btn-primary mx-2" onClick={handlecopyClick}>Copy</button>
+        <button className="btn btn-primary mx-2" onClick={handlermspceClick}>Remove extra space</button>
     </div>
+    <div className="container my-3">
+      <h2>Your Text Summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008* text.split(" ").length} Minutes read</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
